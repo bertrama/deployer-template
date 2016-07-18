39ad6dd4044f4ad803a28d65734cb3f76f92f17c
Colin Gross <grosscol@gmail.com>
2016-07-18 15:25:45 -0400

    Bring production template in line with std. setup.

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,14 +7,14 @@ server 'production-server.organization.org',
   roles: %w{app db web},
   primary: true
 
-set :user, 'myapp'
+set :user, 'myapp-production'
 set :stage, :production
 set :rails_env, 'production'
-set :deploy_to, '/hydra/myapp-production/app'
-set :default_env, { 'HOME' => '/hydra/myapp-production/app/shared'}
+set :deploy_to, '/hydra/myapp-production'
+set :default_env, { 'HOME' => '/hydra/myapp-production/shared'}
 set :log_level, :debug
 set :keep_releases, 3
-set :rbenv_custom_path, '/usr/local/rbenv'
+set :rbenv_custom_path, '/l/local/rbenv'
 set :rbenv_ruby, '2.3.0'
 set :rbenv_type, :system
 set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
@@ -25,7 +25,7 @@ set :config_files, %w{config/database.yml config/secrets.yml     config/puma.rb 
                       config/redis.yml    config/resque-pool.yml config/role_map.yml config/arkivo.yml
                       config/mcommunity.yml config/browse_everything_providers.yml}
 
-set :linked_dirs,  %w{log tmp/pids tmp/cache tmp/sockets tmp/derivatives tmp/uploads vendor/bundle public/system public/uploads}
+set :linked_dirs,  %w{log bundle tmp/pids tmp/cache tmp/derivatives tmp/uploads tmp/sockets vendor/bundle public/system public/uploads}
 
 set :bundle_path, -> { shared_path.join('vendor/bundle') }
 set :bundle_flags, '--quiet --deployment'
@@ -33,13 +33,12 @@ set :bundle_flags, '--quiet --deployment'
 namespace :deploy do
   task :restart do
     on roles(:web) do
-      # This has to be enabled in visudo on the server
-      execute :sudo, '/bin/systemctl', 'restart', 'app-cc-puma@myapp-production.service'
+      # This has to be enabled using visudo on the server
+      execute :sudo, '/bin/systemctl', 'restart', 'app-puma@myapp-production.service'
     end
   end
 
   # Set the stage so config_files gets the correct stage
-  set :stage, 'production'
   before :starting, "linked_files:upload:files"
   after :finishing, :compile_assets
   after :finishing, :cleanup

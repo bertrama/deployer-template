91aaa8da5ddcc5a1f0d9ef8f4bce98cdcb778d43
Colin Gross <grosscol@gmail.com>
2016-07-18 15:08:15 -0400

    Add setup and permissions task   Create shared dirs if not extant.   Update permissions for application group if possible.

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,9 +5,6 @@
 
 lock '~> 3.4'
 
-# Set umask so files are group writeable.
-SSHKit.config.umask = '0002'
-
 set :application, 'myapp'
 set :repo_url, 'https://github.com/myorganization/myapp.git'
 set :scm, :git
@@ -17,10 +14,47 @@ set :use_sudo, false
 set :deploy_via, :remote_cache
 set :rbenv_map_bins, %w{rake gem bundle ruby rails}
 
+# Support multiple users running the deployment (not simultaneously though!!)
+# Set umask so files are group writeable.
+SSHKit.config.umask = '0002'
+# Set :tmp_dir so multiple users can run the deplyment
+set :tmp_dir, File.join('/tmp', ENV['USER'] )
+
 # Use branch from env if provided.
 set :branch, ENV['branch'] || ask('Enter the deployment branch or tag:', 'master')
 
 namespace :deploy do
+  task :create_dirs do
+    on roles(:web) do
+      dirs = linked_dirs(shared_path) << File.join(shared_path, "config")
+      dirs.each do |dir|
+        execute('mkdir', '-p', dir) unless File.exist?(dir)
+      end
+    end
+  end
+
+  task :chmod_dirs do
+    on roles(:web) do
+      dirs = linked_dirs(shared_path) << File.join(shared_path, "config")
+      dirs.each do |dir|
+        execute('chmod', '2775', dir) if File.stat(dir).owned?
+      end
+    end
+  end
+
+  task :chmod_linked_files do
+    on roles(:web) do
+      files = linked_files(shared_path)
+      files.each do |file|
+        execute('chmod', '0660', file) if File.stat(file).owned?
+      end
+    end
+  end
+
+  before :starting, :create_dirs
+  before :starting, :chmod_dirs
+  after :updated, :chmod_assets
+
   after :restart, :clear_cache do
     on roles(:web), in: :groups, limit: 3, wait: 10 do
       # Here we can do anything such as:
@@ -34,7 +68,7 @@ end
 namespace :puma do
   task :start do
     on roles (fetch(:puma_role)) do |role|
-      execute 'sudo', 'systemctl', 'restart', 'app-myapp'
+      execute 'sudo', 'systemctl', 'restart', 'app-puma@myapp.service'
     end
   end
 end
